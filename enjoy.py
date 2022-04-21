cddd384a004215ebfdb4027d3258c957fd08e854
YRE <gongyuhe1023@gmail.com>
2022-04-21 13:46:37 +0200

    some changes

--- enjoy.py
+++ enjoy.py
@@ -21,7 +21,7 @@ algo = "promp_td3"
 #env = env_id + '3'
 env = "Ant-v0"
 env_id = env
-path = "logs/promp_td3/" + env + "_18"
+path = "logs/promp_td3/" + env + "_9"
 
 #env_id = "ALRReacherBalance-v3"
 #path = "logs/promp_td3/ALRReacherBalance-v3_2"
@@ -37,26 +37,29 @@ data['path'] = path
 #data['continue_path'] = "logs/promp_td3/" + env + "_13"
 
 # make the environment
-stats_file = 'env_normalize.pkl'
-stats_path = os.path.join(path, stats_file)
+#stats_file = 'env_normalize.pkl'
+#stats_path = os.path.join(path, stats_file)
 env = gym.make("alr_envs:" + env)
 algo_path = path + "/best_model.npz"
 a = path + "/pos_features.npz"
-print("path",algo_path)
-algorithm = np.load("logs/promp_td3/Ant-v0_18/algo_mean.npz")#"logs/promp_td3/Ant-v0_1/best_model.npy", allow_pickle=True)
-pos_feature = np.load(a)
-vel_feature = np.load(path + "/vel_features.npz")
 algo_path = path + "/algo_mean.npz"
+#a = []
+import pickle
+#with open("my_file.pkl", "wb") as h:
+#    pickle.dump(a, h)
+#open(algo_path, 'rb').close()
 
-algorithm = np.load(algo_path)
+#print("a",h)
+#data = np.array(algo_path )
+algorithm = np.load(algo_path, encoding='bytes', allow_pickle=True)
 for i in algorithm:
     algorithm = np.array(algorithm[i])
 
-for i in pos_feature:
-    pos_feature = np.array(pos_feature[i])
+#for i in pos_feature:
+#    pos_feature = np.array(pos_feature[i])
 
-for i in vel_feature:
-    vel_feature = np.array(vel_feature[i])
+#for i in vel_feature:
+#    vel_feature = np.array(vel_feature[i])
 #"alr_envs:" + env
 # make the model and save the model
 ALGOS = {
@@ -70,13 +73,10 @@ ALGOS = {
         'promp_td3': ProMPTD3
 }
 ALGO = ALGOS[algo]
+#print("critic", data['algo_params'])
 critic = data['algo_params']['policy']
 promp_policy_kwargs = data['promp_params']
 print(env)
-#assert 1==123
-#nv, eval_env = env_continue_load(data)
-#model_path = os.path.join(data['continue_path'], 'model')
-#model = ProMPTD3.continue_load(model_path, tensorboard_log=data['path'], env=env)
 
 model = ALGO(critic, env, seed=1,  initial_promp_params=0.1,  verbose=1,
              trajectory_noise_sigma=0.3, promp_policy_kwargs=promp_policy_kwargs,
@@ -93,12 +93,10 @@ if data['algorithm'] == "td3":
         env.render()
     env.close()
 elif data['algorithm'] == "promp_td3":
-    #algorithm = np.zeros(25).reshape(5,5)
+
     n_actions = env.action_space.shape[-1]
     noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0 * np.ones(n_actions))
-    #noise = action_noise()
-    #algorithm += noise.reshape(7, 5)
-    #print("noise", noise)
+
     print("algo", algorithm)
-    #assert 1==123
-    model.load(algorithm, env, noise, pos_feature=pos_feature, vel_feature=vel_feature)
+
+    model.load(algorithm, env, noise, pos_feature=algorithm, vel_feature=algorithm)
