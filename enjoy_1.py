ff4acde3e8b1d2ad5603f91131644137042554c3
YRE <gongyuhe1023@gmail.com>
2022-04-28 14:15:25 +0200

    some changes

--- enjoy_1.py
+++ enjoy_1.py
@@ -55,7 +55,7 @@ env.reset()
 #algorithm[:, 8] = 0.00006 * np.ones(algorithm[:, 2].shape)
 #algorithm = 0.01 * np.random.rand(basis_num, env.action_space.shape[0])
 
-model = ALGO(critic, env, seed=1,  initial_promp_params=1, critic_network_kwargs=critic_kwargs, verbose=1,
+model = ALGO(critic, env, seed=1,  initial_promp_params=0.1, critic_network_kwargs=critic_kwargs, verbose=1,
              noise_sigma=0.1, promp_policy_kwargs=promp_policy_kwargs,
              critic_learning_rate=data["algo_params"]['critic_learning_rate'],
              actor_learning_rate=data["algo_params"]['actor_learning_rate'], basis_num=data['promp_params']['num_basis'],
