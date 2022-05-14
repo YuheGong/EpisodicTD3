bd497744efb9cfbe49a044683eaee6618b2cb8e8
YRE <gongyuhe1023@gmail.com>
2022-05-14 22:19:37 +0200

    some hcnages

--- model/schedule.py
+++ model/schedule.py
@@ -47,7 +47,7 @@ class dmcCheetahDens_v0_schedule(Schedule):
         #    model.actor_learning_rate = 0.00001
         #    model.actor_optimizer.param_groups[0]['lr'] = model.actor_learning_rate
         if model.eval_reward > 160: #and model.eval_reward < 150:
-            model.actor_learning_rate = 0.0000001
+            model.actor_learning_rate = 0.000001
             model.actor_optimizer.param_groups[0]['lr'] = model.actor_learning_rate
             model.noise_sigma = 0.1
             model.noise = NormalActionNoise(mean=np.zeros(model.dof), sigma=model.noise_sigma * np.ones(model.dof))
