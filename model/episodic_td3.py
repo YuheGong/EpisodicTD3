878da24b8fa673f029907c57e5c9896201541242
YRE <gongyuhe1023@gmail.com>
2022-05-14 14:10:33 +0200

    some changes

--- model/episodic_td3.py
+++ model/episodic_td3.py
@@ -419,7 +419,7 @@ class EpisodicTD3(BaseAlgorithm):
         # Rescale the action from [low, high] to [-1, 1]
         if isinstance(self.action_space, gym.spaces.Box):
             scaled_action = self.policy.scale_action(unscaled_action).reshape(1, self.dof)
-            scaled_action = np.clip(scaled_action, -1, 1)
+            #scaled_action = np.clip(scaled_action, -1, 1)
             buffer_action = scaled_action
             action = self.policy.unscale_action(scaled_action)
             #action = unscaled_action
