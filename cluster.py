d3a2df032cfbddd0a8568766bcf3dbbe1295cc71
YRE <gongyuhe1023@gmail.com>
2022-07-01 10:33:01 +0200

    some chanegs

--- cluster.py
+++ cluster.py
@@ -1,7 +1,7 @@
 import os
 
 for i in range(5):
-    str = f'python enjoy_1.py --env ALRReacherBalanceIP-v3 --seed {i}'
+    str = f'python train.py --env Meta-pick-place-v2 --seed {i}'
     os.system(str)
-    str = f'python enjoy_1.py --env ALRReacherBalanceIP-v4 --seed {i}'
-    os.system(str)
+    #str = f'python enjoy_1.py --env ALRReacherBalanceIP-v4 --seed {i}'
+    #os.system(str)
