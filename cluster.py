f51a1a39fbd0a2b5a8934a5c2c496cd7e8f71153
YRE <gongyuhe1023@gmail.com>
2022-08-01 17:25:54 +0200

    some changes

--- cluster.py
+++ cluster.py
@@ -1,7 +1,8 @@
 import os
 
 for i in range(5):
-    str = f'python train.py --env Meta-pick-place-v2 --seed {i}'
+    str = f' python train.py --e Meta-context-dense-soccer-v2 --c 1 --seed {i}'
+    #str = f' python train.py --e HopperXYJumpStepContext-v0 --c 1 --seed {i}'
     os.system(str)
     #str = f'python enjoy_1.py --env ALRReacherBalanceIP-v4 --seed {i}'
     #os.system(str)
